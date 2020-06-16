d999cdaf07894eeb4a212a04af961e0a6ba697a3
Ping <umitcan.yc@gmail.com>
2020-06-16 22:22:36 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## What is my ip?
+[▶ Sample image](https://python.is-inside.me/QpFGuU7F.png)
---
+++ main.js
@@ -0,0 +1,22 @@
+const URL = 'https://www.imdb.com/search/title/?title_type=feature&groups=top_100';
+fetch('http://ip-api.com/json')
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("body > div > div.ip").innerHTML = data.query;
+        document.querySelector("body > div > div.country").innerHTML = `${data.country} (${data.countryCode})`
+        document.querySelector("body > div > div.isp").innerHTML = data.isp;
+    });
+document.querySelector("div.info").addEventListener('click', () => {
+    const text = document.querySelector("body > div > div.ip").innerHTML;
+    const data = document.querySelector("body > div.info");
+    const hasAlert = document.querySelector("body > div.info > div.alert")
+    if (text !== "Loading info..." && hasAlert === null) {
+        navigator.clipboard.writeText(text).then(function () {
+            data.innerHTML += '<div class="alert">Successfully copied to clipboard.</div>';
+            setTimeout(() => document.querySelector('body > div > div.alert').remove(), 1800);
+        }, function (err) {
+            data.innerHTML += '<div class="alert err">Successfully copied to clipboard.</div>';
+            setTimeout(() => document.querySelector('body > div > div.alert').remove(), 1800);
+        });
+    }
+})
